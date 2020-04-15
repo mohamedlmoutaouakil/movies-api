9df52fa4450383ea3ca126d1c76545169073fe8d
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-04-15 15:10:14 -0600

    Added a dummy version of get movie by id endpoint

--- src/movies.py
+++ src/movies.py
@@ -1,6 +1,6 @@
 
-movies_list = [
-    {
+movies_list = {
+    1: {
         "id": 1,
         "name": 'Fight Club',
         "description": 'An office worker meets a soap maker and started an underground fight club.',
@@ -20,8 +20,33 @@ movies_list = [
                 }
             ],
         "director": 'David Fincher'
+    },
+    2: {
+        "id": 2,
+        "name": 'The Equalizer',
+        "description": 'McCall gives up violence and wishes to lead a quiet and undisturbed life. However, brutal events force him to once again take up the fight for justice.',
+        "duration": 132,
+        "poster": "https://www.movienewsletters.net/photos/140326R1.jpg",
+        "rating": 7.2,
+        "year": 2014,
+        "genre": 
+            [
+                {
+                    "id": 1,
+                    "name": 'Action'
+                },
+                {
+                    "id": 3,
+                    "name": 'Thriller'
+                }
+            ],
+        "director": 'Antoine Fuqua'
     }
-]
+}
 
 def read():
-    return movies_list
+    return movies_list.values()
+
+def get_movie(movie_id):
+    if movie_id in movies_list.keys():
+        return movies_list[movie_id]
