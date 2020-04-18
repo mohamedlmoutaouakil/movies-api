666455b28efbe75da76549097d15e8735f85c03e
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-04-17 21:00:03 -0600

    Fixed serialization issues in endpoints

--- src/movies.py
+++ src/movies.py
@@ -3,10 +3,11 @@ from src.app import db
 
 def read():
     all_movies = Movie.query.all()
-    return all_movies
+    all_movies_dicts = [m.dump() for m in all_movies]
+    return all_movies_dicts
 
 def get_movie(movie_id):
     movie = Movie.query.get(movie_id)
     if movie == None:
         return 'Movie not found', 404
-    return movie
+    return movie.dump()
