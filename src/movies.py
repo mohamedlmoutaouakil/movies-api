6481d997c6c1fe040a4cd85691f065e918b3656e
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-04-17 21:01:03 -0600

    Added openapi specs & code for add_movie endpoint

--- src/movies.py
+++ src/movies.py
@@ -1,5 +1,6 @@
 from src.models import Movie, Genre
 from src.app import db
+import connexion
 
 def read():
     all_movies = Movie.query.all()
@@ -11,3 +12,13 @@ def get_movie(movie_id):
     if movie == None:
         return 'Movie not found', 404
     return movie.dump()
+
+def add_movie(movie):
+    movie_model = movie
+    if connexion.request.is_json:
+        movie_model = Movie.load(movie)
+
+    db.session.add(movie_model)
+    db.session.commit()
+
+    return {}, 201
