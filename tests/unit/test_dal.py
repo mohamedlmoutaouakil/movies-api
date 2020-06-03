d6440e501fa955b2246d186f4ea177a2be2fcc0b
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-06-03 13:04:37 +0100

    Added unit tests for replace_movie_in_db in DAL

--- tests/unit/test_dal.py
+++ tests/unit/test_dal.py
@@ -1,5 +1,5 @@
 import pytest
-from src.dal import get_all_movies, get_movie_by_id, add_movie_to_db
+from src.dal import get_all_movies, get_movie_by_id, add_movie_to_db, replace_movie_in_db
 from src.models import Movie, Genre
 from src.app import db
 
@@ -81,3 +81,34 @@ def test_add_movie_to_db(setup_db):
     # ASSERT
     assert len(all_movies) == 1
     assert all_movies[0].dump() == movie_dict_example_1
+
+def test_replace_movie_in_db_with_existing_id(setup_db):
+    # ARRANGE
+    # Insert a movie in database
+    movie_model = Movie.load(movie_dict_example_1)
+    db.session.add(movie_model)
+    db.session.commit()
+
+    # ACT
+    replace_movie_in_db(1, movie_dict_example_2)
+    # Retrieve the replaced movie
+    all_movies = Movie.query.all()
+    # Change movie_dict_example_2's id to 1 (id of movie to replace)
+    movie_dict_example_2['id'] = 1
+
+    # ASSERT
+    assert len(all_movies) == 1
+    assert all_movies[0].dump() == movie_dict_example_2
+
+def test_replace_movie_in_db_with_non_existing_id(setup_db):
+    # ARRANGE
+    # ACT
+    replace_movie_in_db(2, movie_dict_example_2)
+    # Retrieve the replaced movie
+    all_movies = Movie.query.all()
+    # Change movie_dict_example_2's id to 1 (id is autoincrement => first movie inserted will have id 1)
+    movie_dict_example_2['id'] = 1
+
+    # ASSERT
+    assert len(all_movies) == 1
+    assert all_movies[0].dump() == movie_dict_example_2
