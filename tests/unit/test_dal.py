c6767b01a329a7bb762f3e26163fafaeee1a3b8c
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-06-03 12:33:36 +0100

    Added unit test for get_movie_by_id in DAL

--- tests/unit/test_dal.py
+++ tests/unit/test_dal.py
@@ -1,11 +1,11 @@
 import pytest
-from src.dal import get_all_movies
+from src.dal import get_all_movies, get_movie_by_id
 from src.models import Movie, Genre
 from src.app import db
 
-movie_dict_example = {
-    "description": "Description",
-    "director": "Director",
+movie_dict_example_1 = {
+    "description": "Description1",
+    "director": "Director1",
     "duration": 100,
     "genre": [
       {
@@ -14,9 +14,26 @@ movie_dict_example = {
       }
     ],
     "id": 1,
-    "name": "Movie Example",
-    "poster": "Poster URL",
+    "name": "Movie Example 1",
+    "poster": "Poster URL 1",
     "rating": 7.0,
+    "year": 2019
+  }
+
+movie_dict_example_2 = {
+    "description": "Description2",
+    "director": "Director2",
+    "duration": 120,
+    "genre": [
+      {
+        "id": 1,
+        "name": "Action"
+      }
+    ],
+    "id": 2,
+    "name": "Movie Example 2",
+    "poster": "Poster URL 2",
+    "rating": 8.0,
     "year": 2020
   }
 
@@ -25,7 +42,7 @@ movie_dict_example = {
 def test_get_all_movies(setup_db):
     # ARRANGE
     # Insert a movie in database
-    movie_model = Movie.load(movie_dict_example)
+    movie_model = Movie.load(movie_dict_example_1)
     db.session.add(movie_model)
     db.session.commit()
 
@@ -34,4 +51,22 @@ def test_get_all_movies(setup_db):
 
     # ASSERT
     assert len(all_movies) == 1
-    assert all_movies[0] == movie_dict_example
+    assert all_movies[0] == movie_dict_example_1
+
+def test_get_movie_by_id(setup_db):
+    # ARRANGE
+    # Insert movies in db
+    movie_model_1 = Movie.load(movie_dict_example_1)
+    db.session.add(movie_model_1)
+    db.session.commit() # So we can re-use the Genre model inserted above
+    movie_model_2 = Movie.load(movie_dict_example_2) # Instead of inserting new Genre we retrieve the one above from db
+    db.session.add(movie_model_2)
+    db.session.commit()
+
+    # ACT
+    movie1 = get_movie_by_id(1)
+    movie2 = get_movie_by_id(2)
+
+    # ASSERT
+    assert movie1.dump() == movie_dict_example_1
+    assert movie2.dump() == movie_dict_example_2
