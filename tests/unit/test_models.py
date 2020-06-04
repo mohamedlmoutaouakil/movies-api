05625ec2b2c2ef4742b3cc4e29b2fcebf74759ee
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-06-04 21:32:39 +0100

    Added unit test for load method in Movie model

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -9,3 +9,22 @@ def test_genre_load():
 
     # ASSERT
     assert genre_model.name == genre_dict_example.get('name')
+
+# We added setup_db fixture because Movie.load queries the genre table
+# So we need to connect to the database
+def test_movie_load(setup_db):
+    # ARRANGE
+    # ACT
+    movie_model = Movie.load(movie_dict_example_1)
+
+    # ASSERT
+    assert movie_model.name == movie_dict_example_1.get('name')
+    assert movie_model.description == movie_dict_example_1.get('description')
+    assert movie_model.duration == movie_dict_example_1.get('duration')
+    assert movie_model.poster == movie_dict_example_1.get('poster')
+    assert movie_model.rating == movie_dict_example_1.get('rating')
+    assert movie_model.year == movie_dict_example_1.get('year')
+    assert movie_model.director == movie_dict_example_1.get('director')
+    assert movie_model.genre[0].id == None
+    assert movie_model.genre[0].name == movie_dict_example_1.get('genre')[0].get('name')
+
