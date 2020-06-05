8a15d12b6815c252e989f1826594d11918d6c940
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-06-05 18:41:38 +0100

    Added integration tests for get_movie endpoint

--- tests/integration/test_movies.py
+++ tests/integration/test_movies.py
@@ -18,3 +18,33 @@ def test_read(test_client):
     assert response.status_code == 200
     assert len(all_movies) == 1
     assert all_movies[0] == movie_dict_example_1
+
+def test_get_movie_with_existing_id(test_client):
+    # ARRANGE
+    # Insert movies in db
+    movie_model_1 = Movie.load(movie_dict_example_1)
+    db.session.add(movie_model_1)
+    db.session.commit() # So we can re-use the Genre model inserted above
+    movie_model_2 = Movie.load(movie_dict_example_2) # Instead of inserting new Genre we retrieve the one above from db
+    db.session.add(movie_model_2)
+    db.session.commit()
+    movie_id = 1
+
+    # ACT
+    response = test_client.get(f'/movies/{movie_id}')
+    movie_dict = response.json
+
+    # ASSERT
+    assert response.status_code == 200
+    assert movie_dict == movie_dict_example_1
+
+def test_get_movie_with_non_existing_id(test_client):
+    # ARRANGE
+    movie_id = 1
+
+    # ACT
+    response = test_client.get(f'/movies/{movie_id}')
+
+    # ASSERT
+    assert response.status_code == 404
+    assert response.json == 'Movie Not Found'
